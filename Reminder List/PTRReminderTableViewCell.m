9addafa250a1bdc0173de9be3ed06024a4f53231
Wu Jiashi <wujiashi@Wu-Jiashis-MacBook-Pro.local>
2014-07-09 16:47:53 +0800

    Formatted dueTime label for overdue format

--- Reminder List/PTRReminderTableViewCell.m
+++ Reminder List/PTRReminderTableViewCell.m
@@ -33,7 +33,7 @@
 
 #pragma mark display methods
 
--(void)showControlBar
+- (void)showControlBar
 {
     /*[UIView beginAnimations:@"Fade In" context:nil];
     [UIView setAnimationDelay:10];
@@ -45,46 +45,61 @@
     //[UIView commitAnimations];
 }
 
--(void)hideControlBar
+- (void)hideControlBar
 {
     [self.controlBar setHidden:YES];
 }
 
--(NSString*)formatHours:(int)hours{
+- (NSString*)formatHours:(int)hours{
     NSString *h = hours > 1 ? @"hours" : @"hour";
-    return [NSString stringWithFormat:@"%d %@", hours, h];
+    return [NSString stringWithFormat:@"%d %@ ", hours, h];
 }
 
--(NSString*)formatMinutes:(int)minutes{
+- (NSString*)formatMinutes:(int)minutes{
     NSString *m = minutes > 1 ? @"minutes" : @"minute";
-    return [NSString stringWithFormat:@"%d %@", minutes, m];
+    return [NSString stringWithFormat:@"%d %@ ", minutes, m];
 }
 
--(void)formatDueTimeFromDate:(NSDate *)dueDate
+- (void)formatDueTimeFromDate:(NSDate *)dueDate
 {
     NSTimeInterval periodTillDue = [dueDate timeIntervalSinceDate:[NSDate date]];
+    NSMutableString *dueDateText = [[NSMutableString alloc]init];
     
-    if (periodTillDue < 60 * 60 * 5) {
+    if (periodTillDue < 0) {
+        dueDateText = [NSMutableString stringWithString:@"Overdued "];
+    }
+    
+    if (abs(periodTillDue) < 60 * 60 * 5) {
+        if (periodTillDue < 0) {
+            periodTillDue *= -1;
+            if (periodTillDue < 60) {
+                self.dueDate.text = @"Now";
+                return;
+            }
+        } else {
+            // Make countdown timing nicer, especially in last 1 minute
+            periodTillDue += 60;
+        }
+        
         int hours = periodTillDue / 60.0 / 60;
         int minutes = (periodTillDue - hours * 60 * 60)/60;
         
-        if (periodTillDue < 60 * 60) {
-            self.dueDate.text = [self formatMinutes:minutes];
-        } else {
-                self.dueDate.text = minutes > 0 ?
-            [NSString stringWithFormat:@"%@ %@",[self formatHours:hours],[self formatMinutes:minutes]]:
-            [self formatHours:hours];
+        if (hours > 0) {
+            [dueDateText appendString:[self formatHours:hours]];
         }
-        return;
+        if (minutes > 0) {
+            [dueDateText appendString:[self formatMinutes:minutes]];
+        }
+    } else {
+        NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+        if (periodTillDue < 60 * 60 * 24 * 7){
+            [dateFormatter setDateFormat: @"HH:mm  —  EEEE"];
+        }
+        else {
+            [dateFormatter setDateFormat:@"HH:mm  —  dd MMMM"];
+        }
+        [dueDateText appendString:[dateFormatter stringFromDate: dueDate]];
     }
-    
-    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
-    if (periodTillDue < 60 * 60 * 24 * 7){
-        [dateFormatter setDateFormat: @"HH:mm  —  EEEE"];
-    }
-    else {
-        [dateFormatter setDateFormat:@"HH:mm  —  dd MMMM"];
-    }
-    self.dueDate.text = [dateFormatter stringFromDate: dueDate];
+    self.dueDate.text = dueDateText;
 }
 @end
